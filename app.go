28e061621920e98da1b0c170bbf43749aec0da29
Guillermo Valentín <masweb@me.com>
2026-02-20 10:51:26 +0100

    restore mistakes

--- app.go
+++ app.go
@@ -2,26 +2,154 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"database/sql"
+	"embed"
+	"log"
+
+	db "gvnotes/db/generated"
+	"gvnotes/internal/config"
+	"gvnotes/internal/controllers"
+	"gvnotes/internal/dto"
+	"gvnotes/internal/services"
 )
 
-// App struct
+// appMigrations is set from main.go before NewApp() is called.
+var appMigrations embed.FS
+
+// App is the Wails application struct. It acts as a facade over internal
+// controllers, exposing methods to the frontend via Wails bindings.
 type App struct {
-	ctx context.Context
+	ctx      context.Context
+	sqlDB    *sql.DB
+	auth     *controllers.AuthController
+	notebook *controllers.NotebookController
+	note     *controllers.NoteController
+	image    *controllers.ImageController
 }
 
-// NewApp creates a new App application struct
+// NewApp creates a new App application struct.
 func NewApp() *App {
 	return &App{}
 }
 
-// startup is called when the app starts. The context is saved
-// so we can call the runtime methods
+// startup is called when the app starts. Dependencies are composed here.
 func (a *App) startup(ctx context.Context) {
 	a.ctx = ctx
+
+	sqlDB, err := config.OpenDB(appMigrations)
+	if err != nil {
+		log.Fatalf("failed to open database: %v", err)
+	}
+	a.sqlDB = sqlDB
+
+	imagesDir, err := config.ImagesDir()
+	if err != nil {
+		log.Fatalf("failed to resolve images directory: %v", err)
+	}
+
+	querier := db.New(sqlDB)
+
+	a.auth = controllers.NewAuthController(services.NewAuthService(querier))
+	a.notebook = controllers.NewNotebookController(services.NewNotebookService(querier))
+	a.note = controllers.NewNoteController(services.NewNoteService(querier))
+	a.image = controllers.NewImageController(services.NewImageService(querier, imagesDir))
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+// shutdown is called when the app closes.
+func (a *App) shutdown(_ context.Context) {
+	if a.sqlDB != nil {
+		a.sqlDB.Close()
+	}
+}
+
+// --- Auth ---
+
+func (a *App) GetAuthStatus() interface{} {
+	return a.auth.GetAuthStatus()
+}
+
+func (a *App) SetPassword(password string) interface{} {
+	return a.auth.SetPassword(password)
+}
+
+func (a *App) VerifyPassword(password string) interface{} {
+	return a.auth.VerifyPassword(password)
+}
+
+// --- Notebooks ---
+
+func (a *App) ListNotebooks(parentID string) ([]dto.NotebookListItem, error) {
+	return a.notebook.ListNotebooks(parentID)
+}
+
+func (a *App) GetNotebook(id string) (dto.NotebookDetail, error) {
+	return a.notebook.GetNotebook(id)
+}
+
+func (a *App) CreateNotebook(req dto.CreateNotebookRequest) (dto.NotebookDetail, error) {
+	return a.notebook.CreateNotebook(req)
+}
+
+func (a *App) UpdateNotebookTitle(id string, req dto.UpdateNotebookTitleRequest) (dto.NotebookDetail, error) {
+	return a.notebook.UpdateNotebookTitle(id, req)
+}
+
+func (a *App) UpdateNotebookPosition(id string, req dto.UpdatePositionRequest) error {
+	return a.notebook.UpdateNotebookPosition(id, req)
+}
+
+func (a *App) DeleteNotebook(id string) error {
+	return a.notebook.DeleteNotebook(id)
+}
+
+// --- Notes ---
+
+func (a *App) ListNotes(notebookID string) ([]dto.NoteListItem, error) {
+	return a.note.ListNotes(notebookID)
+}
+
+func (a *App) GetNote(id string) (dto.NoteDetail, error) {
+	return a.note.GetNote(id)
+}
+
+func (a *App) CreateNote(req dto.CreateNoteRequest) (dto.NoteDetail, error) {
+	return a.note.CreateNote(req)
+}
+
+func (a *App) UpdateNoteTitle(id string, req dto.UpdateNoteTitleRequest) (dto.NoteDetail, error) {
+	return a.note.UpdateNoteTitle(id, req)
+}
+
+func (a *App) UpdateNoteContent(id string, req dto.UpdateNoteContentRequest) (dto.NoteDetail, error) {
+	return a.note.UpdateNoteContent(id, req)
+}
+
+func (a *App) UpdateNotePosition(id string, req dto.UpdatePositionRequest) error {
+	return a.note.UpdateNotePosition(id, req)
+}
+
+func (a *App) DeleteNote(id string) error {
+	return a.note.DeleteNote(id)
+}
+
+// --- Images ---
+
+func (a *App) ListImagesByNote(noteID string) ([]dto.ImageItem, error) {
+	return a.image.ListImagesByNote(noteID)
+}
+
+func (a *App) GetImage(id string) (dto.ImageItem, error) {
+	return a.image.GetImage(id)
+}
+
+func (a *App) SaveImage(noteID string, mimeType string, data []byte) (dto.ImageItem, error) {
+	return a.image.SaveImage(noteID, mimeType, data)
+}
+
+func (a *App) DeleteImage(id string) error {
+	return a.image.DeleteImage(id)
+}
+
+func (a *App) GetImagePath(filename string) (string, error) {
+	return a.image.GetImagePath(filename)
 }
--- main.go
+++ main.go
@@ -11,20 +11,24 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed db/migrations
+var migrations embed.FS
+
 func main() {
-	// Create an instance of the app structure
+	appMigrations = migrations
+
 	app := NewApp()
 
-	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "GVNotes",
+		Title:  "gvnotes",
 		Width:  1600,
-		Height: 900,
+		Height: 1200,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
+		OnShutdown:       app.shutdown,
 		Bind: []interface{}{
 			app,
 		},
